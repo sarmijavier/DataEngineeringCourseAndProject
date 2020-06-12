255c3b31430f11331de1373c08d4da51c6b0ce35
sarmijavier <mayayahuarcani@gmail.com>
2020-06-12 00:18:37 -0500

    save the data in a csv file, the escraper is completed

--- web_scraper_curso_data_eng/main.py
+++ web_scraper_curso_data_eng/main.py
@@ -2,6 +2,8 @@ import argparse
 import logging
 import re
 import pdb
+import datetime
+import csv
 logging.basicConfig(level=logging.INFO)
 
 from requests.exceptions import HTTPError
@@ -26,9 +28,27 @@ def _news_scraper(news_site_uid):
         if article:
             logger.info('Article fetched!!')
             articles.append(article)
-            print(article.title)
+            break
 
-    print(len(articles))
+    _save_articles(news_site_uid,articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid,
+        datetime=now
+    )
+
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name, 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 
 def _fetch_article(news_site_uid, host, link):
